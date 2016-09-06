353cc53551606a9879834cb8fa86942c15211abe
uugengiven <lange.john.m@gmail.com>
2016-09-06 14:11:13 -0400

    Add simplified roman kata

---
+++ Roman Numerals/C#/Simplified/RomanNumeralKata01/RomanNumeralKata01/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RomanNumeralKata01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            Console.WriteLine("Enter your number to be converted: ");
+            input = Console.ReadLine();
+            int userInt = Convert.ToInt32(input);
+
+            string result = ConvertToRoman(userInt);
+            Console.WriteLine(input + " written in Roman Numerals is " + result);
+            Console.ReadLine();
+        }
+
+        static string ConvertToRoman(int number)
+        {
+            string roman = "";
+            while (number >= 100)
+            {
+                number = number - 100;
+                roman = roman + "C";
+            }
+            if (number >= 90)
+            {
+                number = number - 90;
+                roman = roman + "XC";
+            }
+            if (number >= 50)
+            {
+                number = number - 50;
+                roman = roman + "L";
+            }
+            if (number >= 40)
+            {
+                number = number - 40;
+                roman = roman + "XL";
+            }
+            while (number >= 10)
+            {
+                number = number - 10;
+                roman = roman + "X";
+            }
+            if (number >= 9)
+            {
+                number = number - 9;
+                roman = roman + "IX";
+            }
+            if (number >= 5)
+            {
+                number = number - 5;
+                roman = roman + "V";
+            }
+            if (number >= 4)
+            {
+                number = number - 4;
+                roman = roman + "IV";
+            }
+            while (number >= 1)
+            {
+                number = number - 1;
+                roman = roman + "I";
+            }
+            return roman;
+        }
+    }
+}
